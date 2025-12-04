00ec8deb65579cc2cd5646202289d52e119ad65f
Vasilii Kasatov <Vetquer@yandex.ru>
2025-12-04 14:42:12 +0300

    fixed tests

--- tests/test_api_create_order.py
+++ tests/test_api_create_order.py
@@ -7,24 +7,26 @@ from data import OrderResponse as correct_r
 
 class TestApiOrder():
 
-    @allure.title("Проверка создания заказа с авторизацией")
-    @allure.description("Проверка создания заказа с авторизацией и ингредиентами")
-    def test_create_order_auth_user_success(self, token_user):
+    @allure.title("Успешное создание заказа с авторизацией и ингредиентами")
+    def test_create_order_auth_user_with_ingr_success(self, token_user):
         token = token_user
         r = OrderApi.create(OrderData.PAYLOAD_CORRECT, token)
         assert r.status_code == correct_r.CODE_SUCCESS_CREATE and r.json()["name"] == correct_r.BODY_SUCCESS_CREATE
 
-    @allure.title("Проверка создания заказа с авторизацией")
-    @allure.description("Проверка появления ошибки при создании заказа с авторизацией и телом запроса {ingredient}")
-    @pytest.mark.parametrize('ingredient, code, body', OrderData.TEST_ORDER_ERROR)
-    def test_create_order_wrong_body_error(self, token_user, ingredient, code, body):
+    @allure.title("Появляеться ошибка при создании заказа с авторизацией и без ингредиентов")
+    def test_create_order_auth_user_without_ingr_error(self, token_user):
         token = token_user
-        r = OrderApi.create(ingredient, token)
-        assert r.status_code == code and r.json()["message"] == body
+        r = OrderApi.create(OrderData.PAYLOAD_EMPTY, token)
+        assert r.status_code == correct_r.CODE_ERROR_EMPTY and r.json()["message"] == correct_r.BODY_ERROR_EMPTY
 
-    @allure.title("Проверка создания заказа без авторизации")
-    @allure.description("Проверка появления ошибки при создании заказа без авторизации")
-    def test_create_order_not_auth_user_error(self):
+    @allure.title("Появляеться ошибка при создании заказа с авторизацией и неверным хешем ингредиентов")
+    def test_create_order_auth_user_wrong_hesh_ingr_error(self, token_user):
+        token = token_user
+        r = OrderApi.create(OrderData.PAYLOAD_INCORRECT, token)
+        assert r.status_code == correct_r.CODE_ERROR_INCORRECT
+
+    @allure.title("Появляеться ошибка при создании заказа без авторизации и с ингредиентами")
+    def test_create_order_not_auth_user_with_ingr_error(self):
         token = ""
         r = OrderApi.create(OrderData.PAYLOAD_CORRECT, token)
         assert r.status_code == correct_r.CODE_ERROR_NOT_AUTH and r.json()["message"] == correct_r.BODY_ERROR_NOT_AUTH

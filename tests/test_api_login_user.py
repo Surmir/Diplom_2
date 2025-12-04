19b7d4ee10d07b147655c61e50e008e60b1e6964
Vasilii Kasatov <Vetquer@yandex.ru>
2025-12-04 14:14:38 +0300

    fixed description test_user_login_success

--- tests/test_api_login_user.py
+++ tests/test_api_login_user.py
@@ -8,7 +8,8 @@ from data import UserLoginResponse as correct_r
 class TestApiUserLogin():
 
     @allure.title("Проверка авторизации пользователя под существующим пользователем")
-    @allure.description("Пользователь авторизуется если все даннные валидны, обязательные поля заполнены")
+    @allure.description("Пользователь авторизуется если поля email и password " \
+    "заполнены данными существующего пользователя")
     def test_user_login_success(self):
         r = UserApi.login(UserData.PAYLOAD_LOGIN)
         assert r.status_code == correct_r.CODE_SUCCESS and correct_r.BODY_SUCCESS in r.json()["accessToken"]
